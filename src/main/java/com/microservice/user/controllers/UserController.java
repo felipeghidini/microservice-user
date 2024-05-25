7da777293a89b55509b74a01b22bf9b259564683
felipeghidini <lipeghidini18@gmail.com>
2024-05-24 21:28:44 -0300

    feat: add get method in controller

--- src/main/java/com/microservice/user/controllers/UserController.java
+++ src/main/java/com/microservice/user/controllers/UserController.java
@@ -2,22 +2,26 @@ package com.microservice.user.controllers;
 
 import com.microservice.user.dtos.UserRecordDto;
 import com.microservice.user.models.UserModel;
+import com.microservice.user.repositories.UserRepository;
 import com.microservice.user.services.UserService;
 import jakarta.validation.Valid;
+import org.apache.catalina.User;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UserController {
 
     final UserService userService;
+    final UserRepository userRepository;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, UserRepository userRepository) {
         this.userService = userService;
+        this.userRepository = userRepository;
     }
 
     @PostMapping("/users")
@@ -26,4 +30,10 @@ public class UserController {
         BeanUtils.copyProperties(userRecordDto, userModel); // recebe um dto e converte em um model
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(userModel));
     }
+
+    @GetMapping("/users")
+    public List<UserModel> findAllUsers() {
+        return  userRepository.findAll();
+    }
+
 }
