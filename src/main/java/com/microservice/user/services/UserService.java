1b597e627364e08d713589c7d6de87ad0dc8adde
felipeghidini <lipeghidini18@gmail.com>
2024-05-29 06:56:01 -0300

    feat: adicionado a config do broker e fila e foi incluso o email dto

--- src/main/java/com/microservice/user/services/UserService.java
+++ src/main/java/com/microservice/user/services/UserService.java
@@ -1,6 +1,7 @@
 package com.microservice.user.services;
 
 import com.microservice.user.models.UserModel;
+import com.microservice.user.producers.UserProducer;
 import com.microservice.user.repositories.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
@@ -9,13 +10,17 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     final UserRepository userRepository;
+    final UserProducer userProducer;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, UserProducer userProducer) {
         this.userRepository = userRepository;
+        this.userProducer = userProducer;
     }
 
-    @Transactional
+    @Transactional // Essa Annotation garante o rollback caso um dos processos falhe
     public UserModel save(UserModel userModel) {
-        return  userRepository.save(userModel);
+        userModel = userRepository.save(userModel);
+        userProducer.publishMessageEmail(userModel);
+        return  userModel;
     }
 }
